e848c787041d0d6b29fe965ca706c1ed16867a9b
isaactm98 <isaactm98@gmail.com>
2017-07-09 21:48:29 -0400

    Send Emails
    Setup/scripts to send emails using proper Google Authentication Methods

---
+++ send_email/send_email_info.py
@@ -0,0 +1,12 @@
+# Enter your information here
+
+RECEIVING_EMAIL = ''
+SENDING_EMAIL = ''
+
+
+def get_receiving_email():
+    return RECEIVING_EMAIL
+
+
+def get_sending_email():
+    return SENDING_EMAIL
