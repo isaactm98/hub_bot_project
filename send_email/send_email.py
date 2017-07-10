cc0761c27f694f0c566ac78a5a2bde16d54f4a34
isaactm98 <isaactm98@gmail.com>
2017-07-09 22:37:44 -0400

    Email Attachment
    Can now email an attachment (most) that can be read from within gmail.

--- send_email/send_email.py
+++ send_email/send_email.py
@@ -1,4 +1,4 @@
-# found and modified from https://stackoverflow.com/questions/37201250/sending-email-via-gmail-python
+# found and modified from various answers on https://stackoverflow.com/questions/37201250/sending-email-via-gmail-python
 import httplib2
 import os
 import oauth2client
@@ -11,8 +11,10 @@ import mimetypes
 from email.mime.image import MIMEImage
 from email.mime.audio import MIMEAudio
 from email.mime.base import MIMEBase
+from email.mime.application import MIMEApplication
 import argparse
 from send_email_info import get_sending_email, get_receiving_email
+from email import encoders
 
 
 SCOPES = 'https://www.googleapis.com/auth/gmail.send'
@@ -69,68 +71,114 @@ def CreateMessageHtml(sender, to, subject, msgHtml, msgPlain):
     msg['Subject'] = subject
     msg['From'] = sender
     msg['To'] = to
+    msg.attach(MIMEText(msgPlain, 'plain'))
+    msg.attach(MIMEText(msgHtml, 'html'))
     message_as_bytes = msg.as_bytes()  # the message should converted from string to bytes.
     message_as_base64 = base64.urlsafe_b64encode(message_as_bytes)  # encode in base64 (printable letters coding)
     raw = message_as_base64.decode()  # need to JSON serializable (no idea what does it means)
     return {'raw': raw}
 
 
-def createMessageWithAttachment(sender, to, subject, msgHtml, msgPlain, attachmentFile):
+def createMessageWithAttachment(sender, to, subject, message_text_html, message_text_plain, attached_file):
     """Create a message for an email.
 
-    Args:
-      sender: Email address of the sender.
-      to: Email address of the receiver.
-      subject: The subject of the email message.
-      msgHtml: Html message to be sent
-      msgPlain: Alternative plain text message for older email clients
-      attachmentFile: The path to the file to be attached.
+    message_text: The text of the email message.
+    attached_file: The path to the file to be attached.
 
     Returns:
-      An object containing a base64url encoded email object.
+    An object containing a base64url encoded email object.
     """
-    message = MIMEMultipart('mixed')
+
+    ##An email is composed of 3 part :
+        #part 1: create the message container using a dictionary { to, from, subject }
+        #part 2: attach the message_text with .attach() (could be plain and/or html)
+        #part 3(optional): an attachment added with .attach()
+
+    ## Part 1
+    message = MIMEMultipart() #when alternative: no attach, but only plain_text
     message['to'] = to
     message['from'] = sender
     message['subject'] = subject
 
-    messageA = MIMEMultipart('alternative')
-    messageR = MIMEMultipart('related')
+    ## Part 2   (the message_text)
+    # The order count: the first (html) will be use for email, the second will be attached (unless you comment it)
+    message.attach(MIMEText(message_text_html, 'html'))
+    #message.attach(MIMEText(message_text_plain, 'plain'))
 
-    messageR.attach(MIMEText(msgHtml, 'html'))
-    messageA.attach(MIMEText(msgPlain, 'plain'))
-    messageA.attach(messageR)
+    ## Part 3 (attachement)
+    # # to attach a text file you containing "test" you would do:
+    # # message.attach(MIMEText("test", 'plain'))
 
-    message.attach(messageA)
+    #-----About MimeTypes:
+    # It tells gmail which application it should use to read the attachement (it acts like an extension for windows).
+    # If you dont provide it, you just wont be able to read the attachement (eg. a text) within gmail. You'll have to download it to read it (windows will know how to read it with it's extension).
 
-    print("create_message_with_attachment: file:", attachmentFile)
-    content_type, encoding = mimetypes.guess_type(attachmentFile)
+    #-----3.1 get MimeType of attachment
+        #option 1: if you want to attach the same file just specify it’s mime types
 
-    if content_type is None or encoding is not None:
-        content_type = 'application/octet-stream'
-    main_type, sub_type = content_type.split('/', 1)
+        #option 2: if you want to attach any file use mimetypes.guess_type(attached_file)
+
+    my_mimetype, encoding = mimetypes.guess_type(attached_file)
+
+    # If the extension is not recognized it will return: (None, None)
+    # If it's an .mp3, it will return: (audio/mp3, None) (None is for the encoding)
+    #for unrecognized extension it set my_mimetypes to  'application/octet-stream' (so it won't return None again).
+    if my_mimetype is None or encoding is not None:
+        my_mimetype = 'application/octet-stream'
+
+
+    main_type, sub_type = my_mimetype.split('/', 1)# split only at the first '/'
+    # if my_mimetype is audio/mp3: main_type=audio sub_type=mp3
+
+    #-----3.2  creating the attachement
+        #you don't really "attach" the file but you attach a variable that contains the "binary content" of the file you want to attach
+
+        #option 1: use MIMEBase for all my_mimetype (cf below)  - this is the easiest one to understand
+        #option 2: use the specific MIME (ex for .mp3 = MIMEAudio)   - it's a shorcut version of MIMEBase
+
+    #this part is used to tell how the file should be read and stored (r, or rb, etc.)
     if main_type == 'text':
-        fp = open(attachmentFile, 'rb')
-        msg = MIMEText(fp.read(), _subtype=sub_type)
-        fp.close()
-    elif main_type == 'image':
-        fp = open(attachmentFile, 'rb')
-        msg = MIMEImage(fp.read(), _subtype=sub_type)
-        fp.close()
-    elif main_type == 'audio':
-        fp = open(attachmentFile, 'rb')
-        msg = MIMEAudio(fp.read(), _subtype=sub_type)
-        fp.close()
-    else:
-        fp = open(attachmentFile, 'rb')
-        msg = MIMEBase(main_type, sub_type)
-        msg.set_payload(fp.read())
-        fp.close()
-    filename = os.path.basename(attachmentFile)
-    msg.add_header('Content-Disposition', 'attachment', filename=filename)
-    message.attach(msg)
+        print("text")
+        temp = open(attached_file, 'r')  # 'rb' will send this error: 'bytes' object has no attribute 'encode'
+        attachement = MIMEText(temp.read(), _subtype=sub_type)
+        temp.close()
 
-    return {'raw': base64.urlsafe_b64encode(message.as_string())}
+    elif main_type == 'image':
+        print("image")
+        temp = open(attached_file, 'rb')
+        attachement = MIMEImage(temp.read(), _subtype=sub_type)
+        temp.close()
+
+    elif main_type == 'audio':
+        print("audio")
+        temp = open(attached_file, 'rb')
+        attachement = MIMEAudio(temp.read(), _subtype=sub_type)
+        temp.close()
+
+    elif main_type == 'application' and sub_type == 'pdf':
+        temp = open(attached_file, 'rb')
+        attachement = MIMEApplication(temp.read(), _subtype=sub_type)
+        temp.close()
+
+    else:
+        attachement = MIMEBase(main_type, sub_type)
+        temp = open(attached_file, 'rb')
+        attachement.set_payload(temp.read())
+        temp.close()
+
+    #-----3.3 encode the attachment, add a header and attach it to the message
+    encoders.encode_base64(attachement)  #https://docs.python.org/3/library/email-examples.html
+    filename = os.path.basename(attached_file)
+    attachement.add_header('Content-Disposition', 'attachment', filename=filename) # name preview in email
+    message.attach(attachement)
+
+
+    ## Part 4 encode the message (the message should be in bytes)
+    message_as_bytes = message.as_bytes() # the message should converted from string to bytes.
+    message_as_base64 = base64.urlsafe_b64encode(message_as_bytes) #encode in base64 (printable letters coding)
+    raw = message_as_base64.decode()  # need to JSON serializable (no idea what does it means)
+    return {'raw': raw}
+
 
 def main():
     msgHtml = "Hi<br/>Html Email"
@@ -140,13 +188,12 @@ def main():
 
     parser.add_argument('to', nargs='?', help='The email address you are sending the email to.',
                         default=get_receiving_email())
-    parser.add_argument('sender', nargs='?',help='Sender of email.', default=get_sending_email())
-    parser.add_argument('subject', nargs='?',help='Subject of email.', default='Default Subject')
+    parser.add_argument('sender', nargs='?', help='Sender of email.', default=get_sending_email())
+    parser.add_argument('subject', nargs='?', help='Subject of email.', default='Default Subject')
+    parser.add_argument('file', nargs='?', help='File to attach to email.', default=None)
     args = parser.parse_args()
 
-    #SendMessage(args.sender, args.to, args.subject, msgHtml, msgPlain)
-    print(args.to)
-    print(args.sender)
+    SendMessage(args.sender, args.to, args.subject, msgHtml, msgPlain, args.file)
 
 
 if __name__ == '__main__':
